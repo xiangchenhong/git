4c9db2fb851dfb40fc1d72c34e8fdbcf59180dbf
xiangCH <xiangCH@njust.com>
2023-05-24 15:10:31 +0800

    second commit

--- src/main/java/com/changan/GitTest01.java
+++ src/main/java/com/changan/GitTest01.java
@@ -3,5 +3,6 @@ package com.changan;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("徘徊过多少橱窗，住过多少旅馆，才会觉得分离也并不冤枉");
+        System.out.println("感情是用来浏览，还是用来珍藏，好让日子天天都过得难忘");
     }
 }
